05fa510c43cd772d6ab1c44424c0ba63687676ea
ElRuncho <francorafae@gmail.com>
2022-03-30 07:06:11 -0600

    adding budgets

--- nabp.py
+++ nabp.py
@@ -1,18 +1,57 @@
 from email.policy import default
 import click
+import boto3
+
+class Config(object):
+    def __init__(self):
+        self.profile = 'default'
+        self.session = boto3.Session(profile_name=self.profile)
+
+
+pass_config = click.make_pass_decorator(Config, ensure=True)
 
 @click.group()
-def cli():
-    click.echo("nabp")
-
+@click.option('--profile', help='perfil preferido de awscli')
+@pass_config
+def cli(config,profile):
+    if profile is None:
+        profile='default'
+    config.profile = profile
+    click.echo("nabp ")
 
 @cli.group('core')
-def core():
+@pass_config
+def core(config):
     """comandos seguridad core"""
     
 
-@core.command('desplegar')
+@core.command('seguridad')
 @click.option('--tag', default=None, help="agregar tag a recursos desplegados")
-def coresec(tag):
-    "desplegar medidas core"
-    click.echo("testing de comando desplegar")
+@pass_config
+def coresec(config,tag):
+    "desplegar medidas de seguridad core"
+    sess = config.session
+    
+    analyzerclient = sess.client('accessanalyzer')
+    
+    click.echo('creado bucket')
+
+
+@core.command('presupuesto')
+@click.option('--id', default=None, help="id de la cuenta de aws")
+@click.option('--presupuesto', default=None, help='monto de presupuesto a crear')
+@click.option('--email', default=None, help='email para enviar notificaciones relacionadas con el presupuesto')
+@pass_config
+def presupuesto(config, id, presupuesto):
+    "establece presupuesto y alertas"
+    sess = config.session
+    budget=sess.client("budgets")
+
+    click.echo(" creando budget y alerta asociada")
+
+    budget.create_budget(   
+                            AccountId=id,
+                            Budget={
+                                
+                            }
+                        )
